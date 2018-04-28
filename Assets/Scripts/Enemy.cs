32e2ab8d04644d20012589bb3cd480b57c6e5d8c
artem88 <artemik2010@gmail.com>
2018-04-28 13:28:38 +0300

    Deleted some imported assets (alien-character etc.) Added new game assets

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour {
+
+    private NavMeshAgent navMesh;
+    private SpawnManager _spawnManager;
+    public GameObject targetBase;
+    public int maxHealth = 100;
+    public int health = 100;
+    public Image healthBar;
+
+    [Header("Coins")]
+
+    public GameObject destroyedTargetPrefab;
+
+
+    // Use this for initialization
+    void Start () {
+        _spawnManager = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
+
+        healthBar = transform.Find("EnemyCanvas/HealthBG/Health").GetComponent<Image>();
+
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+            navMesh.SetDestination(targetBase.transform.position); 
+        
+    }
+
+    public bool RandomResult()
+    {
+        int probability = 3;
+        return (Random.Range(0, probability) == 2);
+    }
+
+    public void TakeDamage()
+    {
+        health -= 5;
+        healthBar.fillAmount = (float)health / (float)maxHealth;
+
+        if (health <= 0)
+        {
+            if (RandomResult() == false)
+            {
+                Transform currentTransform = transform;
+                GameObject oneCoin = Instantiate(destroyedTargetPrefab, currentTransform.position, currentTransform.rotation);
+                Destroy(oneCoin, 5.0f);
+            }
+            
+            Destroy(gameObject);
+            _spawnManager.EnemyDefeated();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public float rayLength = 100;
+
+    public Transform playerHead;
+
+    public Material newMat;
+
+    public Text coinsCountText;
+
+    public int coins = 6;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Raycast();
+
+        coinsCountText.text = "Coins: " + coins.ToString();
+    }
+
+    private void Raycast()
+    {
+        Ray ray = new Ray(playerHead.position, playerHead.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(playerHead.position, playerHead.forward * rayLength, Color.white, 1.0f);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("Coin") && Input.GetMouseButtonDown(0))
+            {
+                coins++;
+                Destroy(hit.collider.gameObject);                
+            }
+
+            if (hit.collider.gameObject.CompareTag("Platform"))
+            {
+                hit.collider.gameObject.GetComponent<Renderer>().materials[0] = newMat;
+                return;
+            }
+        }
+    }
+}
